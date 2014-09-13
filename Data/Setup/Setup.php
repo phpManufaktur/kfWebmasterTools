325e02b53d1528456a43c4368b853a92a0df7272
hertsch <ralf.hertsch@phpmanufaktur.de>
2014-09-13 05:29:49 +0200

    updated `@link` references

--- Data/Setup/Setup.php
+++ Data/Setup/Setup.php
@@ -4,7 +4,7 @@
  * WebmasterTools
  *
  * @author Team phpManufaktur <team@phpmanufaktur.de>
- * @link https://kit2.phpmanufaktur.de/flexContent
+ * @link https://kit2.phpmanufaktur.de/WebmasterTools
  * @copyright 2014 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
  * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
  */
